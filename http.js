2af5934dd638b77873a8f4a8c2f64fb2d4495242
zahuang <x.zahuang@gmail.com>
2021-09-30 15:14:49 +0900

    更新端口8000后解决添加用户问题

--- http.js
+++ http.js
@@ -2,8 +2,8 @@ var http = require('http');
 
 var server = http.createServer();
 
-server.listen(9000,function(){
-    console.log('启动成功：9000')
+server.listen(8000,function(){
+    console.log('启动成功：8000')
 })
 
 var router = require('./router');
